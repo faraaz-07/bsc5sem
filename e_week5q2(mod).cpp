196618a7670f1922f1cb28964acac34c0d225e64
faraaz-07 <130065937+faraaz-07@users.noreply.github.com>
2024-10-24 16:19:41 +0530

    Add files via upload

---
+++ e_week5q2(mod).cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#define SIZE 10000000
+
+int main() {
+   int* arr=(int*)malloc(SIZE*sizeof(int));
+    long long sum = 0;
+    double average;
+    int i,num;
+    
+    // Initialize array with values
+    for (i = 0; i < SIZE; i++) {
+        arr[i] = i + 1;
+    }
+    double st=omp_get_wtime();
+    //sequetial
+    for (i = 0; i < SIZE; i++) {
+        sum += arr[i];
+    }
+    double et=omp_get_wtime();
+    sum=0;
+    double start_time2 = omp_get_wtime();
+    //parallel
+    #pragma omp parallel 
+	{
+		 num=omp_get_num_threads();
+	
+	
+	#pragma omp for reduction(+:sum)
+    for (i = 0; i < SIZE; i++) {
+        sum += arr[i];
+    }
+}
+    average = (double)sum / SIZE;
+
+    double end_time2 = omp_get_wtime();
+
+    printf("Average: %f\n", average);
+    printf("Execution time sequentially: %f seconds\n", et-st);
+    printf("Execution time with OpenMP: %f seconds\n", end_time2 - start_time2);
+    printf("Speed Up: %f seconds\n", (et-st)/(end_time2 - start_time2));
+    printf("Efficiency:%f\n",((et-st)/(end_time2 - start_time2))/num);
+    return 0;
+}
+
